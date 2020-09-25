fe3fcd800e26ce104f612d8ec38c97f26348e5ec
Neil Liddicott <javaboss@mail.com>
2020-09-25 14:59:40 +0100

    Updates

--- exercises/ninja-level-5/main.go
+++ exercises/ninja-level-5/main.go
@@ -8,7 +8,25 @@ type person struct {
 	favourties []string
 }
 
+type vehicle struct {
+	doors  int
+	colour string
+}
+
+type truck struct {
+	vehicle   vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle vehicle
+	luxury  bool
+}
+
 func main() {
+	// EXERCISE 1
+	fmt.Println("EXERCISE 1")
+
 	neil := person{
 		first:      "Neil",
 		last:       "Liddicott",
@@ -17,7 +35,7 @@ func main() {
 
 	helen := person{
 		first:      "Helen",
-		last:       "Liddicott",
+		last:       "Keys",
 		favourties: []string{"chocolate", "just chocolate!"},
 	}
 
@@ -31,4 +49,37 @@ func main() {
 	for i, v := range neil.favourties {
 		fmt.Printf("Ice Cream [%v] [%v]\n", i, v)
 	}
+
+	// EXERCISE 2
+	fmt.Println("EXERCISE 2")
+	m := map[string]person{
+		neil.last:  neil,
+		helen.last: helen,
+	}
+
+	fmt.Println(m)
+
+	// range over map
+	for k, v := range m {
+		//		fmt.Printf("KEY [%v]\tVALUE[%v]\n", k, v)
+		fmt.Println("KEY:", k)
+		fmt.Println(v.first)
+		fmt.Println(v.last)
+
+		for i, val := range v.favourties {
+			fmt.Println(i, val)
+		}
+	}
+
+	// EXERCISE 3
+	fmt.Println("EXERCISE 3")
+	neilsTruck := truck{
+		vehicle: vehicle{
+			doors:  4,
+			colour: "black",
+		},
+		fourWheel: true,
+	}
+
+	fmt.Println(neilsTruck)
 }
