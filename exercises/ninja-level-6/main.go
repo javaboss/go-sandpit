7068abb0fdca1f605ebe6d64999a477fe2dec175
Neil Liddicott <javaboss@mail.com>
2020-12-29 12:09:00 +0000

    Exercise #1

--- exercises/ninja-level-6/main.go
+++ exercises/ninja-level-6/main.go
@@ -1,4 +1,21 @@
 package main
 
+import "fmt"
+
+func foo() int {
+	return 10
+}
+
+func bar() (int, string) {
+	return 20, "Hello"
+}
+
 func main() {
+	// EXERCISE 1
+	// create a func foo() that returns an int
+	// create a func bar() that returns an int and a string
+	f := foo()
+	b1, b2 := bar()
+	fmt.Println(f)
+	fmt.Println(b1, b2)
 }
