040d184b381637bd7c8a915921a1b22383b328d3
Neil Liddicott <javaboss@mail.com>
2021-02-08 09:37:26 +0000

    Pointers

--- pointers/main.go
+++ pointers/main.go
@@ -12,5 +12,7 @@ func main() {
 	fmt.Printf("%T\n", &a)
 
 	var b *int = &a // re-use same address (pointer to int a  and assign to b)
+	// short declaration would be b = &a
 	fmt.Println(b)
+	fmt.Println(*b)
 }
