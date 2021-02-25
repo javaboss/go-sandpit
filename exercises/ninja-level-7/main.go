0491fddf30b6230d9a93481d813993598dbb05cf
Neil Liddicott <javaboss@mail.com>
2021-02-25 12:13:45 +0000

    Pointers. Exercise 2

--- exercises/ninja-level-7/main.go
+++ exercises/ninja-level-7/main.go
@@ -7,13 +7,27 @@ type person struct {
 	age  int
 }
 
-func changeMe(p person) {
-
+func changeMe(p *person) {
+	(*p).name = "Helen Liddicott"
+	(*p).age = 45
 }
 
 func main() {
+	// EXERCISE 1
 	temperature := 21
 
 	fmt.Println("temp = ", temperature)
 	fmt.Println("temp addr = ", &temperature)
+
+	// EXERCISE 2
+	p1 := person{
+		name: "Neil Liddicott",
+		age:  47,
+	}
+
+	fmt.Println(p1)
+
+	changeMe(&p1)
+
+	fmt.Println(p1)
 }
