f3810e848f7465a96700cf7ee3e63b72124449d6
Neil Liddicott <javaboss@mail.com>
2021-03-02 13:27:42 +0000

    Using JSON

--- json/main.go
+++ json/main.go
@@ -1,27 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type person struct {
-	first string
-	last  string
-	age   int
+	First string
+	Last  string
+	Age   int
 }
 
 func main() {
 	p1 := person{
-		first: "James",
-		last:  "Bond",
-		age:   32,
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
 	}
 
 	p2 := person{
-		first: "Miss",
-		last:  "Moneypenny",
-		age:   27,
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
 	}
 
 	people := []person{p1, p2}
 
 	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(bs))
+	}
 }
