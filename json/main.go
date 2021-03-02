ccbe098780633f9c1a79b44951b598bd116df895
Neil Liddicott <javaboss@mail.com>
2021-03-02 13:15:43 +0000

    Using JSON

--- json/main.go
+++ json/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+type person struct {
+	first string
+	last  string
+	age   int
+}
 
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+	}
+
+	p2 := person{
+		first: "Miss",
+		last:  "Moneypenny",
+		age:   27,
+	}
+
+	people := []person{p1, p2}
 }
