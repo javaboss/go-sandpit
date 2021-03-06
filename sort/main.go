e167751db21059b553527f80e9de1724c34edefb
Neil Liddicott <javaboss@mail.com>
2021-03-06 10:50:03 +0000

    Sorting

--- sort/main.go
+++ sort/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
 	xs := []string{"James", "Q", "M", "Moneypenny", "Dr No"}
 
 	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println(xi)
+
 	fmt.Println(xs)
+
 }
