ee5c8bd6e6b40db106a69c60001fd6be931d4277
Neil Liddicott <javaboss@mail.com>
2021-03-06 11:26:49 +0000

    Sorting custom

--- sort/main.go
+++ sort/main.go
@@ -5,7 +5,29 @@ import (
 	"sort"
 )
 
+type person struct {
+	name string
+	age  int
+}
+
+type byAge []person
+
+func (a byAge) Len() int           { return len(a) }
+func (a byAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byAge) Less(i, j int) bool { return a[i].age < a[j].age }
+
+type byName []person
+
+func (bn byName) Len() int           { return len(bn) }
+func (bn byName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn byName) Less(i, j int) bool { return bn[i].name < bn[j].name }
+
+func (p person) String() string {
+	return fmt.Sprintf("%s:%d", p.name, p.age)
+}
+
 func main() {
+	// SORTING ints and strings
 	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
 	xs := []string{"James", "Q", "M", "Moneypenny", "Dr No"}
 
@@ -16,4 +38,20 @@ func main() {
 	fmt.Println(xs)
 	sort.Strings(xs)
 	fmt.Println(xs)
+
+	// Custom Sorting
+	p1 := person{"James", 32}
+	p2 := person{"Moneypenny", 27}
+	p3 := person{"Q", 64}
+	p4 := person{"M", 56}
+
+	people := []person{p1, p2, p3, p4}
+
+	fmt.Println(people)
+	sort.Sort(byAge(people))
+	fmt.Println(people)
+
+	fmt.Println(people)
+	sort.Sort(byName(people))
+	fmt.Println(people)
 }
