bc4921fa01ce6b91483503db1099cabeb4c4246c
Neil Liddicott <javaboss@mail.com>
2021-03-07 11:38:04 +0000

    encryption with bcrypt

--- bcrypt/main.go
+++ bcrypt/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	s := "password1234"
+
+	fmt.Println("PRE-ENCRYPT: ", s)
+
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
+
+	if err != nil {
+		fmt.Println("There was an error", err)
+	} else {
+		fmt.Println("POST-ENCRYPT: ", bs)
+	}
 }
