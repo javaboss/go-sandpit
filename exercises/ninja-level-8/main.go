f0d5ec5abc2e9afe7a93b6088f67bdf90e2b6538
Neil Liddicott <javaboss@mail.com>
2021-03-08 09:39:49 +0000

    Exercise 3 - JSON encode to stdout out with customer encoder

--- exercises/ninja-level-8/main.go
+++ exercises/ninja-level-8/main.go
@@ -5,34 +5,38 @@ import (
 	"fmt"
 )
 
-type user struct {
-	First string
-	Age   int
-}
-
-type unmarshalledUser []struct {
-	First string `json:"First"`
-	Age   int    `json:"Age"`
-}
-
 func main() {
+
+}
+
+func exercise1and2() {
+	type person struct {
+		First string
+		Age   int
+	}
+
+	type unmarshalledPerson []struct {
+		First string `json:"First"`
+		Age   int    `json:"Age"`
+	}
+
 	// EXERCISE 1 - JSON Marshal
-	u1 := user{
+	u1 := person{
 		First: "James",
 		Age:   32,
 	}
 
-	u2 := user{
+	u2 := person{
 		First: "Moneypenny",
 		Age:   27,
 	}
 
-	u3 := user{
+	u3 := person{
 		First: "M",
 		Age:   54,
 	}
 
-	users := []user{u1, u2, u3}
+	users := []person{u1, u2, u3}
 
 	fmt.Println(users)
 	bs, err := json.Marshal(users)
@@ -48,7 +52,7 @@ func main() {
 	// this is using a generated slice of struct from https://mholt.github.io/json-to-go/
 	// you could just do var newUsers []user
 
-	var newUsers unmarshalledUser
+	var newUsers unmarshalledPerson
 	err = json.Unmarshal(bs, &newUsers)
 
 	if err != nil {
@@ -61,4 +65,54 @@ func main() {
 			fmt.Println(user)
 		}
 	}
+}
+
+func exercise3() {
+	// EXERCISE 3 -
+	type user struct {
+		First   string
+		Last    string
+		Age     int
+		Sayings []string
+	}
+
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	// your code goes here
+
 }
