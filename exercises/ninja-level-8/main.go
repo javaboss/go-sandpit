f62939695950b65ccd7cf75f434cdb86533f79e7
Neil Liddicott <javaboss@mail.com>
2021-03-08 14:41:49 +0000

    Exercise 4 - sorting

--- exercises/ninja-level-8/main.go
+++ exercises/ninja-level-8/main.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
 	exercise1and2()
 	exercise3()
+	exercise4()
 }
 
 func exercise1and2() {
@@ -123,4 +125,19 @@ func exercise3() {
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
+}
+
+func exercise4() {
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xs := []string{"random", "rainbow", "delights", "in", "torpedo", "summers", "under", "gallantry", "fragmented", "moons", "across", "magenta"}
+
+	fmt.Println(xi)
+	// sort xi
+	sort.Ints(xi)
+	fmt.Println(xi)
+
+	fmt.Println(xs)
+	// sort xs
+	sort.Strings(xs)
+	fmt.Println(xs)
 }
