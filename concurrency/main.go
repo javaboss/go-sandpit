0dd8040edd656532e7b677543a40419453d6ce19
Neil Liddicott <javaboss@mail.com>
2021-03-09 09:18:14 +0000

    Concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println("OS\t", runtime.GOOS)
-	fmt.Println("ARCH\t", runtime.GOARCH)
-	fmt.Println("CPUs\t", runtime.NumCPU())
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
 	fmt.Println("Goroutines\t", runtime.NumGoroutine())
 	foo()
 	bar()
