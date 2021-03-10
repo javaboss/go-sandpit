eb0426a10d23d34ab8a32264c6e403bf762efb7d
Neil Liddicott <javaboss@mail.com>
2021-03-10 09:31:14 +0000

    Concurrency - atomic

--- atomic/main.go
+++ atomic/main.go
@@ -10,28 +10,8 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	// WaitGroup
-	fmt.Println("OS\t\t", runtime.GOOS)
-	fmt.Println("ARCH\t\t", runtime.GOARCH)
-	fmt.Println("CPUs\t\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
 
-	wg.Add(1)
-	go foo()
-	bar()
-
-	fmt.Println("CPUs\t\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
-	wg.Wait()
-
-	// Channels
-	ch := make(chan int)
-	go func() {
-		ch <- doSomething(5)
-	}()
-	fmt.Println(<-ch)
-
-	// Race Condition
+	// Atomic
 	fmt.Println("CPUs\t\t", runtime.NumCPU())
 	fmt.Println("Goroutines\t", runtime.NumGoroutine())
 
@@ -55,21 +35,4 @@ func main() {
 	wg.Wait()
 
 	fmt.Println("Counter:", counter)
-}
-
-func foo() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("foo:", i)
-	}
-	wg.Done()
-}
-
-func bar() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("bar:", i)
-	}
-}
-
-func doSomething(x int) int {
-	return x * 5
 }
