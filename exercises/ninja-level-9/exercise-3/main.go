ee2f59af34bd0c638b9f01a3ad69da670aaf85eb
Neil Liddicott <javaboss@mail.com>
2021-03-15 16:16:18 +0000

    Exercise 3 - Concurrency

---
+++ exercises/ninja-level-9/exercise-3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	counter := 0
+	wg.Add(100)
+
+	var mlock sync.Mutex
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			mlock.Lock()
+			counter++
+			mlock.Unlock()
+			runtime.Gosched()
+			fmt.Println("Func Counter:", counter)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Final Counter:", counter)
+}
