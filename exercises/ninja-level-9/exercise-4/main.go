4ec483df80a861a88bfff5015b2c7d863468d655
Neil Liddicott <javaboss@mail.com>
2021-03-16 18:11:20 +0000

    Exercise 4 - Concurrency with mutex

---
+++ exercises/ninja-level-9/exercise-4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	counter := 0
+	wg.Add(100)
+
+	var mlock sync.Mutex
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			mlock.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			mlock.Unlock()
+			fmt.Println("Func Counter:", counter)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Final Counter:", counter)
+}
