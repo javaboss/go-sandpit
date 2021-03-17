820e56de29d95e6ebaf8823ec064e3fa88f1dfb9
Neil Liddicott <javaboss@mail.com>
2021-03-17 15:46:10 +0000

    Directional Channels - more examples

---
+++ channels/03-directional-with-funcs/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	c := make(chan int)
+
+	// send
+	go send(c)
+
+	// receive
+	receive(c) // could use waitgroups - however, not executing this func as a goroutine ensures that it blocks until send is complete
+
+	fmt.Println("about to exit ")
+}
+
+func send(c chan<- int) {
+	c <- 42
+}
+
+func receive(c <-chan int) {
+	fmt.Println(<-c)
+}
