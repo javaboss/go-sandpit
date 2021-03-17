ec408942dc65804229529105c3534fbd6c944cae
Neil Liddicott <javaboss@mail.com>
2021-03-17 15:28:47 +0000

    Directional Channels - general to specific conversions

--- channels/02-directional-channels/main.go
+++ channels/02-directional-channels/main.go
@@ -11,4 +11,9 @@ func main() {
 	fmt.Printf("c\t%T\n", c)
 	fmt.Printf("cr\t%T\n", cr)
 	fmt.Printf("cs\t%T\n", cs)
+
+	// general to specific converts
+	fmt.Println("-----")
+	fmt.Printf("c\t%T\n", (<-chan int)(c))
+	fmt.Printf("c\t%T\n", (chan<- int)(c))
 }
