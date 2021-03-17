1a9a31140be6f2e3c3ca21a70f72301a9229b4dd
Neil Liddicott <javaboss@mail.com>
2021-03-17 13:45:30 +0000

    Directional Channels - send or receive only exmaples

--- channels/02-directional-channels/main.go
+++ channels/02-directional-channels/main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	c := make(chan int, 2)
 
+	// c := make(<-chan int, 2) // RECEIVE only channel - example
+	// c := make(chan<- int, 2) // SEND only channel - example
+
 	c <- 42
 	c <- 43
 
