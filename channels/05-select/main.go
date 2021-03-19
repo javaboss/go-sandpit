e8ba5fd89b8259645dbc1e880869ef674bcc5eca
Neil Liddicott <javaboss@mail.com>
2021-03-19 09:58:34 +0000

    Concurrency - Select

--- channels/05-select/main.go
+++ channels/05-select/main.go
@@ -1,5 +1,25 @@
 package main
 
 func main() {
+	even := make(chan int)
+	odd := make(chan int)
+	quit := make(chan int)
 
+	// send
+	go send(even, odd, quit)
+
+	// receive
+}
+
+func send(e, o, q chan<- int) {
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	close(e)
+	close(o)
+	q <- 0
 }
