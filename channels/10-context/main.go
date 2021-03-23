25d95eade6e31ec1f349d610daa3af3719866810
Neil Liddicott <javaboss@mail.com>
2021-03-23 10:24:59 +0000

    Concurrency - context with cancel

--- channels/10-context/main.go
+++ channels/10-context/main.go
@@ -13,10 +13,18 @@ func main() {
 	fmt.Printf("Context type\t%T\t\n", ctx)
 	fmt.Println("-----------------")
 
-	ctx, _ = context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)
 
 	fmt.Println("context:\t", ctx)
 	fmt.Println("context err:\t", ctx.Err())
 	fmt.Printf("context type:\t%T\n", ctx)
 	fmt.Println("----------")
+
+	cancel()
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context err:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("----------")
+
 }
