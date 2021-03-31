ce10469a5f26cb12f35df71d5afa55ce5d707b9f
Neil Liddicott <javaboss@mail.com>
2021-03-31 09:40:19 +0100

    Exercise 1 - channels using either unbuffered + go routine or buffered only

---
+++ exercises/ninja-level-10/exercise-1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	/*
+		In this case, you need to use a go routine and execute an annonymous function to stop the channe blocking.
+		Otherwise, use a buffered channel i.e. c := make (chan int, 1) - the go routine is no longer needed
+	*/
+
+	c := make(chan int)
+
+	go func() {
+		c <- 42
+	}()
+
+	fmt.Println(<-c)
+}
