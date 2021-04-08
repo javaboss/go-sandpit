65e8a493b2a5be7afa2647b86aa2226c64397886
Neil Liddicott <javaboss@mail.com>
2021-04-08 11:08:33 +0100

    typo fixed

--- exercises/ninja-level-10/exercise-1/main.go
+++ exercises/ninja-level-10/exercise-1/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	/*
-		In this case, you need to use a go routine and execute an annonymous function to stop the channe blocking.
+		In this case, you need to use a go routine and execute an annonymous function to stop the channel blocking.
 		Otherwise, use a buffered channel i.e. c := make (chan int, 1) - the go routine is no longer needed
 	*/
 
